c554f44cd4229af4b9bb5727b9d8bc0e01efcf9e
Joshua-Lake01 <joshualake62@gmail.com>
2024-11-09 17:07:23 +0000

    Initial Commit

--- src/main/java/co2123/hw1/domain/Arcade.java
+++ src/main/java/co2123/hw1/domain/Arcade.java
@@ -1,33 +1,5 @@
 package co2123.hw1.domain;
 
 public class Arcade {
-    private String manufacturer;
-    private String type;
-    private String genre;
-    private int score;
 
-    public void set_manufacturer(String manufacturer){
-        this.manufacturer = manufacturer;
-    }
-    public void set_type(String type){
-        this.type = type;
-    }
-    public void set_genre(String genre){
-        this.genre = genre;
-    }
-    public void set_score(int score){
-        this.score = score;
-    }
-    public String get_manufacturer(){
-        return manufacturer;
-    }
-    public String get_type(){
-        return type;
-    }
-    public String get_genre(){
-        return genre;
-    }
-    public int get_score(){
-        return score;
-    }
 }
