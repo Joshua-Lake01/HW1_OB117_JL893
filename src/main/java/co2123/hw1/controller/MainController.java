e1b609ef4bd1053c676c5dc995dc595fb69f4a8e
Alex <abakal04@gmail.com>
2024-11-09 18:51:25 +0000

    show arcades

--- src/main/java/co2123/hw1/controller/MainController.java
+++ src/main/java/co2123/hw1/controller/MainController.java
@@ -1,11 +1,12 @@
 package co2123.hw1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import co2123.hw1.Hw1Application;
 @Controller
 
 public class MainController {
@@ -17,6 +18,10 @@ public class MainController {
     public String post_request(){
         return "start";
     }
-
+    @GetMapping("/arcades")
+    public String show_arcades(Model model) {
+        model.addAttribute("arcades", Hw1Application.arcades);
+        return "arcades/list";
+    }
 
 }
