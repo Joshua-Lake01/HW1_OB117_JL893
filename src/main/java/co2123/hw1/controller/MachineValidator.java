3aa1bcfdae6ac9e7f259eca1bfc2a4837675c135
Joshua-Lake01 <joshualake62@gmail.com>
2024-11-12 12:08:39 +0000

    Task 6 Machine Validation Almost Complete

--- src/main/java/co2123/hw1/controller/MachineValidator.java
+++ src/main/java/co2123/hw1/controller/MachineValidator.java
@@ -7,13 +7,17 @@ import co2123.hw1.domain.Machine;
 import co2123.hw1.Hw1Application;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MachineValidator implements Validator {
     @Override
     public boolean supports(Class<?> clazz) {return Arcade.class.equals(clazz);}
 
 
     @Override
-    public void validate(Object target, Errors errors, @RequestParam int arcade) {
+    public void validate(Object target, Errors errors) {
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "manufacturer", "", "Machine Needs A Manufacturer");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "type", "", "Machine Needs A Type");
@@ -22,15 +26,14 @@ public class MachineValidator implements Validator {
 
         Machine machine = (Machine) target;
         //get arcades
+        List<String> Genres = new ArrayList<>(Arrays.asList("Racing", "Sports", "Adventure"));
 
-        for(Machine arcade1 : Hw1Application.arcades){
-            if(arcade1.getId() == arcade.getId()){
-                errors.rejectValue("id", "", "Arcade ID Already Exists");
-            }
+        if(!Genres.contains(machine.getGenre())){
+           errors.rejectValue("genre", "", "Enter A Valid Genre");
         }
-        String emailRegex = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
-        if(!arcade.getEmail().matches(emailRegex)) {
-            errors.rejectValue("email", "", "Please enter a valid email address");
+
+        if(!(machine.getScore() > 5000 && machine.getScore() < 1)){
+           errors.rejectValue("score", "", "Score Needs To Be Between 1 and 5000");
         }
 
     }
