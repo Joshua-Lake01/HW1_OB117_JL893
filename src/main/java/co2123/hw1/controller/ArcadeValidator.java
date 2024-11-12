16fe2807edc2e2a54940e6a54bbdda1b555abb3f
Joshua-Lake01 <joshualake62@gmail.com>
2024-11-12 11:47:15 +0000

    Task 6 validation id

--- src/main/java/co2123/hw1/controller/ArcadeValidator.java
+++ src/main/java/co2123/hw1/controller/ArcadeValidator.java
@@ -3,6 +3,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import org.springframework.validation.ValidationUtils;
 import co2123.hw1.domain.Arcade;
+import co2123.hw1.Hw1Application;
 
 public class ArcadeValidator implements Validator {
     @Override
@@ -12,9 +13,18 @@ public class ArcadeValidator implements Validator {
     public void validate(Object target, Errors errors) {
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id", "", "Arcade Needs An ID");
+
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "", "Arcade Needs A Name");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "", "Arcade Needs A Email");
 
+        Arcade arcade = (Arcade) target;
+
+
+        for(Arcade arcade1 : Hw1Application.arcades){
+            if(arcade1.getId() == arcade.getId()){
+                errors.rejectValue("id", "", "Arcade ID Already Exists");
+            }
+        }
 
     }
 }
